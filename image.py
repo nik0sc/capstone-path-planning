23ba6ec47b088efe7f8d6eb7f3b7aa39773f0451
Miguel <migsquizon@yahoo.com>
2020-07-10 20:55:57 +0800

    working cpp i think

--- image.py
+++ image.py
@@ -9,6 +9,15 @@ from collections import defaultdict, namedtuple
 from functools import reduce
 import matplotlib.pyplot as plt
 from pprint import pprint
+#miguel
+import itertools
+import copy
+import networkx as nx
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
 
 """
 You will need: PIL, numpy, networkx, matplotlib, pygraphviz
@@ -650,13 +659,13 @@ def graph_labels(gr: nx.Graph) -> Dict[int, str]:
 
 
 def reeb_process(gr: nx.Graph) -> Sequence[Tuple[int, int, int, int]]:
-    """
-    Convert the Reeb graph from NetworkX object to the preferred representation
-    for the Chinese postman algorithm.
+    # """
+    # Convert the Reeb graph from NetworkX object to the preferred representation
+    # for the Chinese postman algorithm.
 
-    Format:
-    [(crit pt on left, crit pt on right, cell #, pixels in cell), ...]
-    """
+    # Format:
+    # [(crit pt on left, crit pt on right, cell #, pixels in cell), ...]
+    # """
 
     assert gr.is_directed() and gr.is_multigraph(), "Wrong kind of graph"
 
@@ -667,6 +676,218 @@ def reeb_process(gr: nx.Graph) -> Sequence[Tuple[int, int, int, int]]:
 
     return out
 
+def create_complete_graph(pair_weights, flip_weights=True):
+    # """
+    # Create a completely connected graph using a list of vertex pairs and the shortest path distances between them
+    # Parameters: 
+    #     pair_weights: list[tuple] from the output of get_shortest_paths_distances
+    #     flip_weights: Boolean. Should we negate the edge attribute in pair_weights?
+    # """
+    g = nx.Graph()
+    for k, v in pair_weights.items():
+        wt_i = - v if flip_weights else v
+        # g.add_edge(k[0], k[1], {'distance': v, 'weight': wt_i})  # deprecated after NX 1.11 
+        g.add_edge(k[0], k[1], **{'distance': v, 'weight': wt_i})  
+    return g
+
+
+def add_augmenting_path_to_graph(graph, min_weight_pairs):
+# """
+# Add the min weight matching edges to the original graph
+# Parameters:
+#     graph: NetworkX graph (original graph from trailmap)
+#     min_weight_pairs: list[tuples] of node pairs from min weight matching
+# Returns:
+#     augmented NetworkX graph
+# """
+
+# We need to make the augmented graph a MultiGraph so we can add parallel edges
+    graph_aug = nx.MultiGraph(graph.copy())
+    for pair in min_weight_pairs:
+        graph_aug.add_edge(pair[0], 
+                           pair[1], 
+                           **{'distance': nx.dijkstra_path_length(graph, pair[0], pair[1]), 'trail': 'augmented'}
+                           # attr_dict={'distance': nx.dijkstra_path_length(graph, pair[0], pair[1]),
+                           #            'trail': 'augmented'}  # deprecated after 1.11
+                          )
+    return graph_aug
+
+
+def create_eulerian_circuit(graph_augmented, graph_original, starting_node=None):
+    # """Create the eulerian path using only edges from the original graph."""
+    euler_circuit = []
+    naive_circuit = list(nx.eulerian_circuit(graph_augmented, source=starting_node))
+    
+    for edge in naive_circuit:
+        edge_data = graph_augmented.get_edge_data(edge[0], edge[1])    
+        
+        if edge_data[0]['trail'] != 'augmented':
+            # If `edge` exists in original graph, grab the edge attributes and add to eulerian circuit.
+            edge_att = graph_original[edge[0]][edge[1]]
+            euler_circuit.append((edge[0], edge[1], edge_att)) 
+        else: 
+            aug_path = nx.shortest_path(graph_original, edge[0], edge[1], weight='distance')
+            aug_path_pairs = list(zip(aug_path[:-1], aug_path[1:]))
+            
+            print('Filling in edges for augmented edge: {}'.format(edge))
+            print('Augmenting path: {}'.format(' => '.join(str(aug_path))))
+            print('Augmenting path pairs: {}\n'.format(aug_path_pairs))
+            
+            # If `edge` does not exist in original graph, find the shortest path between its nodes and 
+            #  add the edge attributes for each link in the shortest path.
+            for edge_aug in aug_path_pairs:
+                edge_aug_att = graph_original[edge_aug[0]][edge_aug[1]]
+                euler_circuit.append((edge_aug[0], edge_aug[1], edge_aug_att))
+                                      
+    return euler_circuit
+
+def create_cpp_edgelist(euler_circuit):
+    # """
+    # Create the edgelist without parallel edge for the visualization
+    # Combine duplicate edges and keep track of their sequence and # of walks
+    # Parameters:
+    #     euler_circuit: list[tuple] from create_eulerian_circuit
+    # """
+    cpp_edgelist = {}
+
+    for i, e in enumerate(euler_circuit):
+        edge = frozenset([e[0], e[1]])
+
+        if edge in cpp_edgelist:
+            cpp_edgelist[edge][2]['sequence'] += ', ' + str(i)
+            cpp_edgelist[edge][2]['visits'] += 1
+
+        else:
+            cpp_edgelist[edge] = e
+            cpp_edgelist[edge][2]['sequence'] = str(i)
+            cpp_edgelist[edge][2]['visits'] = 1
+        
+    return list(cpp_edgelist.values())
+
+
+
+
+
+def get_shortest_paths_distances(graph, pairs, edge_weight_name):
+    # """Compute shortest distance between each pair of nodes in a graph.  Return a dictionary keyed on node pairs (tuples)."""
+    distances = {}
+    for pair in pairs:
+        distances[pair] = nx.dijkstra_path_length(graph, pair[0], pair[1], weight=edge_weight_name)
+    return distances
+
+
+
+def convert_edge_list_to_pandas(edgelist):
+    df = pd.DataFrame(edgelist)
+    # trail_list = [trail for _ in range(len(edgelist))]
+    df.columns = ['node1','node2','trail','distance']
+    # df.insert(loc=)
+    return df
+
+
+
+
+def chinese_postman(edgelist):
+    g = nx.Graph()
+    for i, elrow in edgelist.iterrows():
+        # g.add_edge(elrow[0], elrow[1], attr_dict=elrow[2:].to_dict())  # deprecated after NX 1.11
+        g.add_edge(elrow[0], elrow[1], **elrow[2:].to_dict())
+
+    # print('# of edges: {}'.format(g.number_of_edges()))
+    # print(elrow[0]) # node1
+    # print(elrow[1]) # node2
+    # print(elrow[2:].to_dict()) # edge attribute dict
+    nodes_odd_degree = [v for v, d in g.degree() if d % 2 == 1]
+    print('Number of nodes of odd degree: {}'.format(len(nodes_odd_degree)))
+    print('Number of total nodes: {}'.format(len(g.nodes())))
+
+    # Compute all pairs of odd nodes. in a list of tuples
+    odd_node_pairs = list(itertools.combinations(nodes_odd_degree, 2))
+
+    # Preview pairs of odd degree nodes
+    # print(odd_node_pairs[0:10])
+
+    # Counts
+    print('Number of odd pairs: {}'.format(len(odd_node_pairs)))
+
+
+    # Compute shortest paths.  Return a dictionary with node pairs keys and a single value equal to shortest path distance.
+    odd_node_pairs_shortest_paths = get_shortest_paths_distances(g, odd_node_pairs, 'distance')
+
+    # Preview with a bit of hack (there is no head/slice method for dictionaries).
+    print(dict(list(odd_node_pairs_shortest_paths.items())[0:10]))
+
+
+        # Generate the complete graph
+    g_odd_complete = create_complete_graph(odd_node_pairs_shortest_paths, flip_weights=True)
+
+    # Counts
+    print('Number of nodes: {}'.format(len(g_odd_complete.nodes())))
+    print('Number of edges: {}'.format(len(g_odd_complete.edges())))
+
+
+    # Compute min weight matching.
+    # Note: max_weight_matching uses the 'weight' attribute by default as the attribute to maximize.
+    odd_matching_dupes = nx.algorithms.max_weight_matching(g_odd_complete, True)
+
+    print('Number of edges in matching: {}'.format(len(odd_matching_dupes)))
+
+
+    odd_dupes = {}
+    for e in odd_matching_dupes:
+        odd_dupes[e[0]] = e[1]
+    # Convert matching to list of deduped tuples
+    odd_matching = list(pd.unique([tuple(sorted([k, v])) for k, v in odd_dupes.items()]))
+
+    # Counts
+    print('Number of edges in matching (deduped): {}'.format(len(odd_matching)))
+
+
+    # Create augmented graph: add the min weight matching edges to g
+    g_aug = add_augmenting_path_to_graph(g, odd_matching)
+
+    # Counts
+    print('Number of edges in original graph: {}'.format(len(g.edges())))
+    print('Number of edges in augmented graph: {}'.format(len(g_aug.edges())))
+
+    naive_euler_circuit = list(nx.eulerian_circuit(g_aug, source=0))
+    print('Length of eulerian circuit: {}'.format(len(naive_euler_circuit)))
+    print(naive_euler_circuit[0:10])
+    
+
+    # Create the Eulerian circuit
+    euler_circuit = create_eulerian_circuit(g_aug, g, 9)
+
+    print('Length of Eulerian circuit: {}'.format(len(euler_circuit)))
+    for i, edge in enumerate(euler_circuit[:]):
+        print(i, edge)
+
+
+    # Computing some stats
+    total_mileage_of_circuit = sum([edge[2]['distance'] for edge in euler_circuit])
+    total_mileage_on_orig_trail_map = sum(nx.get_edge_attributes(g, 'distance').values())
+    _vcn = pd.value_counts(pd.value_counts([(e[0]) for e in euler_circuit]), sort=False)
+    node_visits = pd.DataFrame({'n_visits': _vcn.index, 'n_nodes': _vcn.values})
+    _vce = pd.value_counts(pd.value_counts([sorted(e)[0] + sorted(e)[1] for e in nx.MultiDiGraph(euler_circuit).edges()]))
+    edge_visits = pd.DataFrame({'n_visits': _vce.index, 'n_edges': _vce.values})
+
+    # Printing stats
+    print('Mileage of circuit: {0:.2f}'.format(total_mileage_of_circuit))
+    print('Mileage on original trail map: {0:.2f}'.format(total_mileage_on_orig_trail_map))
+    print('Mileage retracing edges: {0:.2f}'.format(total_mileage_of_circuit-total_mileage_on_orig_trail_map))
+    print('Percent of mileage retraced: {0:.2f}%\n'.format((1-total_mileage_of_circuit/total_mileage_on_orig_trail_map)*-100))
+
+    print('Number of edges in circuit: {}'.format(len(euler_circuit)))
+    print('Number of edges in original graph: {}'.format(len(g.edges())))
+    print('Number of nodes in original graph: {}\n'.format(len(g.nodes())))
+
+    print('Number of edges traversed more than once: {}\n'.format(len(euler_circuit)-len(g.edges())))  
+
+    print('Number of times visiting each node:')
+    print(node_visits.to_string(index=False))
+
+    print('\nNumber of times visiting each edge:')
+    print(edge_visits.to_string(index=False))
 
 if __name__ == "__main__":
     arr, config = load_image("test")
@@ -674,17 +895,21 @@ if __name__ == "__main__":
     graph = build_cell_graph(slices, adjs)
     reeb = build_reeb_graph(graph, adjs)
     # pos = nx.drawing.nx_agraph.pygraphviz_layout(graph, prog="dot")
-    pos = nx.drawing.nx_agraph.pygraphviz_layout(reeb, prog="neato")
+    # pos = nx.drawing.nx_agraph.pygraphviz_layout(reeb, prog="neato")
     # labels = graph_labels(graph)
     # nx.draw(graph, with_labels=True, cmap=plt.cm.Paired, node_color=range(12),
     #         node_size=800, pos=pos, labels=labels)
 
-    nx.draw(reeb, with_labels=True, cmap=plt.cm.Paired, node_color=range(10),
-            node_size=800, pos=pos)
+    # nx.draw(reeb, with_labels=True, cmap=plt.cm.Paired, node_color=range(10),
+    #         node_size=800, pos=pos)
     edge_labels = {(u, v): attrs["cell"]
                    for u, v, attrs in reeb.edges(data=True)}
-    nx.draw_networkx_edge_labels(reeb, pos=pos, font_color="red",
-                                 edge_labels=edge_labels)
-    plt.show()
-    pprint(reeb_process(reeb))
+    # nx.draw_networkx_edge_labels(reeb, pos=pos, font_color="red",
+    #                              edge_labels=edge_labels)
+    # plt.show()
+    edgelist = reeb_process(reeb)
+    edgelist = convert_edge_list_to_pandas(edgelist)
+    chinese_postman(edgelist)
+    
+    # pprint(edge_list)
     print("Break here")
